b98b57a9e7a10a16414b67256e7eab470b128e91
Roman Borisov <rborisoff@gmail.com>
2016-10-30 12:22:15 +0000

    mkpkg

--- mkpkg.sh
+++ mkpkg.sh
@@ -4,6 +4,7 @@ mkdir -p pkg/storage/ca-st-ro/assets
 mkdir -p pkg/storage/ca-st-ro/bin
 
 cp -r etc/* pkg/
+cp assets/castro-* pkg/storage/ca-st-ro/assets/
 cp assets/bkbg.jpg pkg/storage/ca-st-ro/assets/
 cp assets/player.glade pkg/storage/ca-st-ro/assets
 cp build/player pkg/storage/ca-st-ro/bin/
